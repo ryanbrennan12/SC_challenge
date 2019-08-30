d909e3875dc73440135b06f53f2409e79ebd7c7e
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-08-30 13:54:18 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const routes = require('./routes')
+const port = 3000
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+
+app.listen(port, () => console.log(`We are running on port ${port}!!! 🚀`))
